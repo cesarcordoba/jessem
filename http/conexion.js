096f0e32251a236294c34be5c79f688bf32e9779
Your Name <you@example.com>
2018-12-12 13:26:18 -0600

    12 de diciembre 2018 13:26

--- http/conexion.js
+++ http/conexion.js
@@ -3,23 +3,33 @@ var mysql = require('mysql');
 var Sequelize = require('sequelize');
 
 var sequelize = new Sequelize('jessem', 'jessem', '#1q2w3e4r', {
-    host: 'jessem.database.windows.net',
-    dialect: 'mssql',
-    port: '1433',
-    logging : false,
+    host: '35.226.119.103',
+    dialect: 'mysql',
+    port: '3306',
     pool: {
         min: 10,
         max: 300,
         idle: 30000,
-    },
-    dialectOptions: {
-        requestTimeout : 30000,
-        encrypt: true,
-        ssl: {
-            ca: fs.readFileSync('ssl.crt.pem')
-        }
     }
-})
+});
+// var sequelize = new Sequelize('jessem', 'jessem', '#1q2w3e4r', {
+//     host: 'jessem.database.windows.net',
+//     dialect: 'mssql',
+//     port: '1433',
+//     logging : false,
+//     pool: {
+//         min: 10,
+//         max: 300,
+//         idle: 30000,
+//     },
+//     dialectOptions: {
+//         requestTimeout : 30000,
+//         encrypt: true,
+//         ssl: {
+//             ca: fs.readFileSync('ssl.crt.pem')
+//         }
+//     }
+// })
 //
 // sequelize.authenticate().complete(function(err) {
 //     if (err) {
