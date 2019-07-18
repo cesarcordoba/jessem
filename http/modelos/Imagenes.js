edb7b4d1451342b184e2d91beb2e117aa0099bb9
cesar <cesar@starbrand.mx>
2019-07-18 13:26:47 -0500

    subiendo a cloud

--- http/modelos/Imagenes.js
+++ http/modelos/Imagenes.js
@@ -1,6 +1,14 @@
 module.exports = ({Sequelize, sequelize } = conector) =>
     sequelize.define('imagenes', {
-        imagen: Sequelize.TEXT,
+        imagen: {
+            type: Sequelize.BLOB('medium'),
+            get() {
+                var imagenBin = this.getDataValue('imagen');
+                console.log(imagenBin)
+                var Imagenes = new Buffer(imagenBin).toString('ascii');
+                return Imagenes
+            },
+         }
     },{
     	name : {
     		singular: 'imagen',
